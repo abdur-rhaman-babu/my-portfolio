deab7ebea266e720a770cf39cbd8c1cc31cd8b3d
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-02-06 14:08:23 +0600

    banner section done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl mx-auto pt-20">
+      <div className="max-w-7xl lg:mx-auto mt-20 bg-zinc-50 lg:p-10 p-2 md:p-5 -z-50">
         <Outlet />
       </div>
       <Footer />
--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,18 +1,75 @@
-import banner from '../../assets/image/banner.png'
+import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import banner from "../../assets/image/banner.png";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { TypeAnimation } from "react-type-animation";
 const Banner = () => {
   return (
-    <div className='lg:flex items-center justify-between'>
-      <div className='flex flex-col gap-2 max-w-2xl'>
-          <h2 className='font-bold text-3xl'>Hello It's Me</h2>
-          <h1 className='font-bold text-4xl'>Abdur Rhaman Babu</h1>
-          <h2 className='font-bold text-3xl'>I'm A <span>Frontend Web Developer</span></h2>
-          <p>I am a frontend web developer specializing in creating responsive, interactive, and user-friendly websites. Passionate about design and seamless user experiences.</p>
-          <div>
+    <div className="md:flex items-center gap-5 justify-between flex flex-col-reverse md:flex-row">
+      <div className="flex flex-col gap-2 max-w-2xl">
+        <h2 className="font-bold text-3xl">Hello It's Me</h2>
+        <h1 className="font-bold text-4xl">Abdur Rhaman Babu</h1>
 
-          </div>
+        <h2 className="font-bold text-3xl">
+          I'm a {' '}
+          <TypeAnimation
+            sequence={[
+              "Frontend Developer",
+              1000,
+              "React.Js Developer",
+              1000,
+              "UI Designer",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+            className="text-[#058789]"
+          />
+        </h2>
+
+        <p>
+          I am a frontend web developer specializing in creating responsive,
+          interactive, and user-friendly websites. Passionate about design and
+          seamless user experiences.
+        </p>
+        <div className="flex items-center gap-2 my-5">
+          <i className="border-2 border-[#058789] p-1 rounded-lg">
+            <a href="https://github.com/abdur-rhaman-babu">
+              {" "}
+              <FaGithubSquare size={35} />
+            </a>
+          </i>
+          <i className="border-2 border-[#058789] p-1 rounded-lg text-blue-500">
+            <a href="https://www.linkedin.com/in/abdur-rhaman-babu-265148347/">
+              <FaLinkedin size={35} />
+            </a>
+          </i>
+          <i className="border-2 border-[#058789] p-1 rounded-lg">
+            <a href="https://x.com/?lang=en">
+              <FaSquareXTwitter size={35} />
+            </a>
+          </i>
+          <i className="border-2 border-[#058789]  p-1 rounded-lg text-blue-700">
+            <a href="https://www.facebook.com/mdarahman.babu/">
+              <FaFacebookSquare size={35} />
+            </a>
+          </i>
+        </div>
+        <div>
+          <button>
+            {" "}
+            <a
+              href="https://drive.google.com/file/d/1LFuWI93B67g_jyavfsX6m2GTS0rUhbiy/view?usp=sharing"
+              download
+              className="text-white bg-gradient-to-r from-[#058789] to-[#05696B] hover:from-[#05696B] hover:to-[#058789] px-4 py-2 rounded-md font-semibold"
+            >
+              Download CV
+            </a>
+          </button>
+        </div>
       </div>
-      <div>
-          <img src={banner} alt="" />
+      <div className="bg-gradient-to-r from-[#058789] to-[#05696B] rounded-t-full">
+        <img src={banner} alt="" />
       </div>
     </div>
   );
